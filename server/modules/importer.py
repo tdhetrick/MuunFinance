5ddbdfa8e131966399ca04e874f6f9b6fe20fe89
Todd Hetrick <me@toddhetrick.com>
2024-03-27 21:34:59 -0400

    up

--- server/modules/importer.py
+++ server/modules/importer.py
@@ -1,7 +1,19 @@
 from modules.database import db
 from modules.model import  ImportConfig, Transaction
+from datetime import datetime
 
 class TransactionImporter:
+    
+    modifiers = {
+        'to_positive_float': float,
+        'to_negative_float': lambda x: -float(x),
+        'credit_or_debit': lambda x, y: float(y) if x == 'credit' else -float(y),
+        'to_date': lambda x: datetime.strptime(x, '%Y-%m-%d'),  # Adjust date format as needed
+        # ... other modifiers ...
+    }
+    
+    mapping = []
+    
     def __init__(self, file_content,account_id):
         self.file_content = file_content
         self.account_id = account_id
@@ -38,14 +50,29 @@ class TransactionImporter:
     def save_configuration(self):
         config = self.detect_configuration()
         headers = self.detect_header()
+        
+        if not self.mapping:
+            for header in headers:
+                self.add_mapping(header)
+        #mapping = self.mapping
+        #success = self.save_mapping(mapping)  # Save the mapping
+        # if not success:
+        #     return False  # If saving the mapping failed, return False
+
         import_config = ImportConfig(
             account_id=self.account_id,
             delimiter=config['delimiter'],
             date_format='',  # determine the date format
-            column_mapping={header: None for header in headers} if headers else {}
+            column_mapping=self.mapping
         )
         db.session.add(import_config)
-        db.session.commit()
+        try:
+            db.session.commit()
+            return True  # Success
+        except Exception as e:
+            db.session.rollback()
+            print(e)
+            return False  # Failure
 
     def has_existing_config(self):
         existing_config = ImportConfig.query.filter_by(account_id=self.account_id).first()
@@ -87,3 +114,25 @@ class TransactionImporter:
             db.session.rollback()
             print(e)
             return False  # Failure  
+        
+  
+
+    def add_mapping(self, import_field, db_field=None, modifier=None):     
+       
+        self.mapping.append({
+            'import_field': import_field,
+            'db_field': db_field,
+            'modifier': modifier
+        })
+        
+
+    def save_mapping(self, mapping):
+        import_config = ImportConfig(account_id=self.account_id, mapping=mapping)
+        db.session.add(import_config)
+        try:
+            db.session.commit()
+            return True  # Success
+        except Exception as e:
+            db.session.rollback()
+            print(e)
+            return False  # Failure
